23963f7295602a143b60add3d3cc040111baa354
DoraneX <55027295+Katoma610@users.noreply.github.com>
2024-05-30 20:46:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -381,12 +381,3 @@ priceCheckboxes.forEach(checkbox => {
         }
     });
 });
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-    var scrollpos = localStorage.getItem('scrollpos');
-    if (scrollpos) window.scrollTo(0, scrollpos);
-});
-
-window.onbeforeunload = function(e) {
-    localStorage.setItem('scrollpos', window.scrollY);
-};
